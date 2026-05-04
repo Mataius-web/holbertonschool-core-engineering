475afbe3b1d7e03f4fdde263bd1d55a44b6fc019
Mataius-web <nicolas.jacquemin26@gmail.com>
2026-05-04 21:57:12 +0200

    Task 1

---
+++ python_fundamentals/hello_world/structured_output.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+pi = 3.14
+computation = 5 + 5 == 10
+
+print("Language: Python")
+print("Version: 3")
+print(f"Pi approx: {pi:.2f}")
+print(f"Computation valid: {computation}")
