f8a09d4260725fd61799304304e7d60ccb379883
Mataius-web <nicolas.jacquemin26@gmail.com>
2026-05-05 14:56:22 +0200

    Task 1.

--- python_fundamentals/functions_modules/uppercase.py
+++ python_fundamentals/functions_modules/uppercase.py
@@ -4,6 +4,7 @@ def uppercase(str):
     for letter in str:
         if ord(letter) >= ord('a') and ord(letter) <= ord('z'):
             letter = ord(letter) - 32
-            print("{}".format(chr(letter)), end="")
-        else:
-            print("{}".format(letter), end="")
+            letter = chr(letter)
+        print("{}".format(letter), end="")
+
+    print()
