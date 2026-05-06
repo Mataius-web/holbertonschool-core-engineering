385e614191a7efc64c5b6936f48f403ce2aff4f4
Mataius-web <nicolas.jacquemin26@gmail.com>
2026-05-06 23:44:42 +0200

    Task 3.

--- python_fundamentals/core_data_structures/print_matrix_integer.py
+++ python_fundamentals/core_data_structures/print_matrix_integer.py
@@ -2,8 +2,11 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for element in range(0, len(row)):
-            if element == len(row) - 1:
-                print("{:d}".format(row[element]))
-            else:
-                print("{:d}".format(row[element]), end=" ")
+        if len(row) == 0:
+            print()
+        else:
+            for element in range(0, len(row)):
+                if element == len(row) - 1:
+                    print("{:d}".format(row[element]))
+                else:
+                    print("{:d}".format(row[element]), end=" ")
