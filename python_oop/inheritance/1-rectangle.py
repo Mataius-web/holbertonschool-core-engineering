c78232d92530567357003c90755c2ba60ba451cb
Mataius-web <nicolas.jacquemin26@gmail.com>
2026-05-15 15:15:28 +0200

    Add files

---
+++ python_oop/inheritance/1-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module that defines the Rectangle Class"""
+BaseGeometry = __import__('base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class inheriting from BaseGeometry"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
